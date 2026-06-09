425fd8a0685b362f6ef41352d4b4e28fb2d649ba
hashimshaji23 <hashimshaji12@gmail.com>
2026-06-09 13:06:36 +0530

    express js start

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
-const http = require("http")
+// const http = require("http")
+
+// const PORT = 3001;
+
+// const server = http.createServer((req,res)=>{
+//     res.write('hello world')
+//     res.end()
+// })
+// server.listen(PORT,()=>{
+//     console.log(`server is running on ${PORT}`)
+// })
+
+
+const express = require("express")
+const app = express()
 
 const PORT = 3001;
 
-const server = http.createServer((req,res)=>{
-    res.write('hello world')
-    res.end()
+app.get('/',(req,res)=>{
+    res.send('hello Express Js')
 })
-server.listen(PORT,()=>{
-    console.log(`server is running on ${PORT}`)
+
+app.get('/about',(req,res)=>{
+    res.send('About page')
+})
+
+app.get('/contact',(req,res)=>{
+    res.send('contact page')
+})
+
+app.listen(PORT,()=>{
+    console.log(`server is running on port ${PORT}`)
 })
