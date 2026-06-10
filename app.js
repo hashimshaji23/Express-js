15e336dda5a37aa016164553a456405f60941908
hashimshaji23 <hashimshaji12@gmail.com>
2026-06-10 14:36:12 +0530

    express nots

--- app.js
+++ app.js
@@ -16,18 +16,76 @@ const app = express()
 
 const PORT = 3001;
 
-app.get('/',(req,res)=>{
-    res.send('hello Express Js')
+// app.get('/',(req,res)=>{
+//     res.send('hello Express Js')
+// })
+
+// app.get('/about',(req,res)=>{
+//     res.send('About page')
+// })
+
+// app.get('/contact',(req,res)=>{
+//     res.send('contact page')
+// })
+
+// app.post('/addUser',(req,res)=>{
+//     res.send('user data added')
+// })
+
+// app.put('/editUser',(req,res)=>{
+//     res.send('USER data updated')
+// })
+
+// app.delete('/deleteUser',(req,res)=>{
+//     res.send('USER data deleted')
+// })
+
+// app.listen(PORT,()=>{
+//     console.log(`server is running on port ${PORT}`)
+// })
+
+
+
+
+// CRUD operation
+
+// Request methods
+
+// A. req.params
+
+app.get('/user/:id',(req,res)=>{
+    res.send(params.id)
 })
 
-app.get('/about',(req,res)=>{
-    res.send('About page')
+// B. req.query
+
+app.get('/search',(req,res)=>{
+    res.send(req.query.name)
 })
 
-app.get('/contact',(req,res)=>{
-    res.send('contact page')
+// c. req.body
+
+app.use(express.json())
+app.post('/user',(req,res)=>{
+    console.log(req.body)
+})
+
+
+// req.method
+app.get('/search', (req,res)=>{
+    res.send(req.method)
+})
+
+// req.url
+
+app.get('/search', (req,res)=>{
+    res.send(req.url)
+})
+
+app.get('/search',(req,res)=>{
+    res.send(req.header)
 })
 
 app.listen(PORT,()=>{
-    console.log(`server is running on port ${PORT}`)
+    console.log(`server is running on port ${PORT}`);
 })
