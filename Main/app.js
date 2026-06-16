fb4040bff97abc0c327cb4abb8866682bd793122
hashimshaji23 <hashimshaji12@gmail.com>
2026-06-16 13:15:13 +0530

    express main

--- Main/app.js
+++ Main/app.js
@@ -1,9 +1,6 @@
-const express = require("express")
+import express from 'express'
 const app = express()
 
-const PORT = 5000;
-
-
-app.listen(PORT, ()=>{
-    console.log(`server is running on ${PORT}`)
+app.listen(3000, ()=>{
+    console.log(`server is running PORT ${3000}`)
 })
